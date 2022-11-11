ff97bb29be93d0987cb2ff6b96b2ebb0dfda5e11
Dhyaan Shah <80191264+ddS08@users.noreply.github.com>
2022-11-12 02:10:37 +0530

    Update MainActivity.java

--- MAD/time/MainActivity.java
+++ MAD/time/MainActivity.java
@@ -1,56 +1,36 @@
-package com.example.dateandtime;
+package com.example.myapplication;
 
-import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private TimePicker timePicker1;
-    private TextView time;
-    private Calendar calendar;
-    private String format = "";
+import java.sql.Time;
+
+public class MainActivity extends AppCompatActivity {
+    TimePicker tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
-        time = (TextView) findViewById(R.id.textView1);
-        calendar = Calendar.getInstance();
-
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int min = calendar.get(Calendar.MINUTE);
-        showTime(hour, min);
     }
 
-    public void setTime(View view) {
-        int hour = timePicker1.getCurrentHour();
-        int min = timePicker1.getCurrentMinute();
-        showTime(hour, min);
-    }
-
-    public void showTime(int hour, int min) {
-        if (hour == 0) {
-            hour += 12;
-            format = "AM";
-        } else if (hour == 12) {
-            format = "PM";
-        } else if (hour > 12) {
-            hour -= 12;
-            format = "PM";
-        } else {
-            format = "AM";
+    public void onc(View v)
+    {
+        tp = (TimePicker) findViewById(R.id.timep);
+        int min,hrs;
+        min = tp.getCurrentMinute();
+        hrs=tp.getCurrentHour();
+        if (min<10) {
+            Toast.makeText(this, hrs + ":0" + min, Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, hrs + ":" + min, Toast.LENGTH_SHORT).show();
         }
 
-        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
-                .append(" ").append(format));
     }
-
 
 }
