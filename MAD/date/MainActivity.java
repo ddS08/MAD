e2512fe426a39c7aa7943a4e45aa5645b15f7bdb
Dhyaan Shah <80191264+ddS08@users.noreply.github.com>
2022-11-12 02:12:01 +0530

    Update MainActivity.java

--- MAD/date/MainActivity.java
+++ MAD/date/MainActivity.java
@@ -1,56 +1,46 @@
-package com.example.dateandtime;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.TimePicker;
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
-public class MainActivity extends Activity {
-    private TimePicker timePicker1;
-    private TextView time;
-    private Calendar calendar;
-    private String format = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
-        time = (TextView) findViewById(R.id.textView1);
-        calendar = Calendar.getInstance();
+        tv= findViewById(R.id.textv);
+        final Calendar calendar=Calendar.getInstance();
 
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int min = calendar.get(Calendar.MINUTE);
-        showTime(hour, min);
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        tv.setText(date);
+
+                    }
+                },year,month,day);
+                dialog.show();
+            }
+        });
+
     }
-
-    public void setTime(View view) {
-        int hour = timePicker1.getCurrentHour();
-        int min = timePicker1.getCurrentMinute();
-        showTime(hour, min);
-    }
-
-    public void showTime(int hour, int min) {
-        if (hour == 0) {
-            hour += 12;
-            format = "AM";
-        } else if (hour == 12) {
-            format = "PM";
-        } else if (hour > 12) {
-            hour -= 12;
-            format = "PM";
-        } else {
-            format = "AM";
-        }
-
-        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
-                .append(" ").append(format));
-    }
-
-
 }
